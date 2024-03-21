a2c94019ca785c6070394da389a34fc1531051d7
KeerthikaPrem <159062201+KeerthikaPrem@users.noreply.github.com>
2024-03-22 01:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("hi")
+    // variables
+    // let score = 0 //initialization
+    // score = 10.5//assignment
+    // console.log("score is" + score);
+
+    // let amount = 1000
+    // console.log("your bill amount is rs." + amount)
+
+    // const pi = 3.14
+
+    // var val = 10
+    // console.log("Happy ")
+
+    // let name = prompt("what's your name ?")
+    // console.log("hi",name)
+
+    // let a = prompt("Enter a number")
+    // console.log(Number(a)+10)
